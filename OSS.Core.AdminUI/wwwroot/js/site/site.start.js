b28ac2279e8b7c08ac49b76ba0a500fc43884f48
osscore <1985088337@qq.com>
2019-09-26 00:06:55 +0800

    引入vue和验证控件

--- OSS.Core.AdminUI/wwwroot/js/site/site.start.js
+++ OSS.Core.AdminUI/wwwroot/js/site/site.start.js
@@ -2,3 +2,10 @@
     OssPjax.start(isDev);
 }
 //Vue.use(VeeValidate, { events: "blur", locale: "zh_CN" });
+//Vue.component('ValidationProvider', ValidationProvider);
+ 
+VeeValidate.extend('required', VeeValidateRules.required);
+VeeValidate.extend('email', VeeValidateRules.email);
+
+// Register the component globally.
+Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
