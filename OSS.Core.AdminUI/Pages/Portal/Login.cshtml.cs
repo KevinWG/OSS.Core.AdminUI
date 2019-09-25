7f420adb90d79a7026a82c56e23de5560f694c5e
LongX <longxintx@outlook.com>
2019-09-25 18:37:39 +0800

    添加登录注册页面

---
+++ OSS.Core.AdminUI/Pages/Portal/Login.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OSS.Core.AdminUI.Pages.Portal
+{
+    [AllowAnonymous]
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
