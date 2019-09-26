8544b9bbf358b202546e570f26c927e23da8d6b1
osscore <1985088337@qq.com>
2019-09-26 22:04:06 +0800

    删除验证控件

--- OSS.Core.AdminUI/wwwroot/js/site/site.start.js
+++ OSS.Core.AdminUI/wwwroot/js/site/site.start.js
@@ -4,8 +4,3 @@
 //Vue.use(VeeValidate, { events: "blur", locale: "zh_CN" });
 //Vue.component('ValidationProvider', ValidationProvider);
  
-VeeValidate.extend('required', VeeValidateRules.required);
-VeeValidate.extend('email', VeeValidateRules.email);
-
-// Register the component globally.
-Vue.component('ValidationProvider', VeeValidate.ValidationProvider);
