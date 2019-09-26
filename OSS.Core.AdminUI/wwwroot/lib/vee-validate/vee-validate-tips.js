4260b28d577ad1f9fea209a7e97b00070c1d7b07
osscore <1985088337@qq.com>
2019-09-26 23:04:26 +0800

    添加vee验证控件

---
+++ OSS.Core.AdminUI/wwwroot/lib/vee-validate/vee-validate-tips.js
@@ -0,0 +1,12 @@
+﻿if (!!VeeValidate) {
+    // 错误提示信息
+    var dict = {
+        en: {
+            messages: {
+                required: (field) => field + ' 不能为空！',
+                min: (field, e) => field + " 不能少于" + e[0] + "位！"
+            }
+        }
+    };
+    VeeValidate.Validator.updateDictionary(dict);
+}
