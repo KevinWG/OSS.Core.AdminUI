49898ff7d2bb8ef1d855aeeb8fa3aab975c18216
osscore <1985088337@qq.com>
2019-11-13 23:01:47 +0800

    注释

--- OSS.Core.AdminUI/Startup.cs
+++ OSS.Core.AdminUI/Startup.cs
@@ -15,21 +15,20 @@ namespace OSS.Core.AdminUI
         {
             ConfigUtil.Configuration = Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             // 因为需要全站校验是否登录，所以这里是全局处理
-            // 否则接口Controller基类处理即可，所有ajax请求统一处理，授权登录跳转，纯页面元素本身无需校验 
+            // 否则接口Controller基类处理即可，所有ajax请求统一处理，授权登录跳转，纯页面元素本身无需校验
+            // 此处完全可以自定义，按照自己的验证中间件处理
             services.AddControllers(opt => { opt.Filters.Add(new WebAdminAuthAttribute()); })
                 .AddJsonOptions(jsonOpt =>
                 {
                     jsonOpt.JsonSerializerOptions.IgnoreNullValues     = true;
                     jsonOpt.JsonSerializerOptions.PropertyNamingPolicy = null;
                 });
-
             var builder = services.AddRazorPages();
 
 #if DEBUG
